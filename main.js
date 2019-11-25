eb919e9c7d784c826e9d798b9651e00a7fc7a4fc
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-11-25 11:48:37 -0700

    Complete step 4 user experience

--- main.js
+++ main.js
@@ -5,19 +5,58 @@ popup.addEventListener('click', hideHeader);
 function hideHeader(event) {
   console.log(event);
   if (event.target.classList.contains("close")){
-    event.target.parentNode.remove();
+    // event.target.parentNode.remove();
+    popup.classList.add('hide');
   }
 }
 
-var navButton = document.querySelector('#wallet');
-navButton.addEventListener('click', selectWallet);
+var accountScreen = document.querySelector ('#welcomeMain');
+
+var navButtonWallet = document.querySelector('#wallet');
+navButtonWallet.addEventListener('click', selectWallet);
 
 function selectWallet(event){
   console.log(event);
   if (event.target.classList.contains("active")){
-    console.log(nada);
+    console.log("active");
   } else{
-    console.log(nope);
+    navButtonWallet.classList.add('active');
+    navButtonClipboard.classList.remove('active');
+    navButtonProfile.classList.remove('active');
+    accountScreen.classList.add('hide');
+  }
+
+}
+
+var navButtonClipboard = document.querySelector('#clipboard');
+navButtonClipboard.addEventListener('click', selectClipboard);
+
+function selectClipboard(event){
+  console.log(event);
+  if (event.target.classList.contains("active")){
+    console.log("active");
+  } else{
+    navButtonClipboard.classList.add('active');
+    navButtonWallet.classList.remove('active');
+    navButtonProfile.classList.remove('active');
+    accountScreen.classList.remove('hide');
+    popup.classList.remove('hide');
+  }
+
+}
+
+var navButtonProfile = document.querySelector('#profile');
+navButtonProfile.addEventListener('click', selectProfile);
+
+function selectProfile(event){
+  console.log(event);
+  if (event.target.classList.contains('active')){
+    console.log('active');
+  } else{
+    navButtonProfile.classList.add('active');
+    navButtonClipboard.classList.remove('active');
+    navButtonWallet.classList.remove('active');
+
   }
 
 }
